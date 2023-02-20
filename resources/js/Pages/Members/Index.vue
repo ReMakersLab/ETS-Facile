06a1dd0c29c144b1fab62798c53e80741a1355d7
pfumarola <paolo96fumarola@gmail.com>
2023-02-20 18:39:25 +0100

    .

--- resources/js/Pages/Members/Index.vue
+++ resources/js/Pages/Members/Index.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
     <Head title="Members" />
-    <h1 class="mb-8 text-3xl font-bold">Members</h1>
+    <h1 class="mb-8 text-3xl font-bold">Soci</h1>
     <DataTable :value="members" :paginator="true" :rows="10" rowHover showGridlines filterDisplay="menu" :filters="filters" :globalFilterFields="['id', 'name', 'surname', 'fiscal_code', 'email']" selectionMode="single" @rowSelect="onRowSelect">
       <template #header>
         <div class="flex justify-between">
-          <Button type="button" icon="pi pi-filter-slash" label="Clear" class="p-button-outlined" @click="clearFilter()" />
+          <Button type="button" icon="pi pi-filter-slash" label="Svuota filtri" class="p-button-outlined" @click="clearFilter()" />
           <span class="p-input-icon-left">
             <i class="pi pi-search" />
-            <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+            <InputText v-model="filters['global'].value" placeholder="Ricerca" />
           </span>
-          <Button label="New member" icon="pi pi-plus" iconPos="right" @click="$inertia.visit('/members/create')" />
+          <Button label="Nuovo socio" icon="pi pi-plus" iconPos="right" @click="$inertia.visit('/members/create')" />
         </div>
       </template>
       <Column v-for="col of columns" :field="col.field" :header="col.header" :sortable="col.sortable" :filterField="col.field" :key="col.field">
         <template #filter="{ filterModel, filterCallback }">
-          {{ filterModel }}
           <InputText type="text" v-model="filterModel.value" @input="filterCallback()" class="p-column-filter" />
         </template>
       </Column>
@@ -37,15 +36,16 @@ export default {
   components: {
     Head,
   },
+  layout: Layout,
   props: ['members'],
   data() {
     return {
       filters: null,
       columns: [
-        { field: 'id', header: 'Card ID', sortable: true },
-        { field: 'name', header: 'Name', sortable: true },
-        { field: 'surname', header: 'Surname', sortable: true },
-        { field: 'fiscal_code', header: 'Fiscal Code', sortable: true },
+        { field: 'id', header: 'ID', sortable: true },
+        { field: 'name', header: 'Nome', sortable: true },
+        { field: 'surname', header: 'Cognome', sortable: true },
+        { field: 'fiscal_code', header: 'Codice Fiscale', sortable: true },
         { field: 'email', header: 'Email', sortable: true },
       ],
     }
@@ -60,7 +60,7 @@ export default {
     initFilters() {
       this.filters = {
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        id: { value: null, matchMode: FilterMatchMode.EQUALS },
+        id: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
         name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
         surname: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
         fiscal_code: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
@@ -71,6 +71,5 @@ export default {
       this.$inertia.visit('members/'+event.data.id+'/edit');
     },
   },
-  layout: Layout,
 }
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,20 +4,93 @@ import { InertiaProgress } from '@inertiajs/progress'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 
 import PrimeVue from 'primevue/config';
-import Knob from 'primevue/knob';
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import ColumnGroup from 'primevue/columngroup';     //optional for column grouping
-import Row from 'primevue/row';                     //optional for row
-import Chart from 'primevue/chart';
+import AutoComplete from 'primevue/autocomplete';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import Avatar from 'primevue/avatar';
+import AvatarGroup from 'primevue/avatargroup';
+import Badge from 'primevue/badge';
 import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
+import Breadcrumb from 'primevue/breadcrumb';
 import Calendar from 'primevue/calendar';
+import Card from 'primevue/card';
+import CascadeSelect from 'primevue/cascadeselect';
+import Carousel from 'primevue/carousel';
+import Chart from 'primevue/chart';
+import Checkbox from 'primevue/checkbox';
+import Chip from 'primevue/chip';
+import Chips from 'primevue/chips';
+import ColorPicker from 'primevue/colorpicker';
+import Column from 'primevue/column';
 import ConfirmDialog from 'primevue/confirmdialog';
+import ConfirmPopup from 'primevue/confirmpopup';
+import ContextMenu from 'primevue/contextmenu';
+import DataTable from 'primevue/datatable';
+import DataView from 'primevue/dataview';
+import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions';
+import Dialog from 'primevue/dialog';
+import Divider from 'primevue/divider';
+import Dropdown from 'primevue/dropdown';
+import Fieldset from 'primevue/fieldset';
+import FileUpload from 'primevue/fileupload';
+import Galleria from 'primevue/galleria';
+import Image from 'primevue/image';
+import InlineMessage from 'primevue/inlinemessage';
+import Inplace from 'primevue/inplace';
+import InputSwitch from 'primevue/inputswitch';
+import InputText from 'primevue/inputtext';
+import InputMask from 'primevue/inputmask';
+import InputNumber from 'primevue/inputnumber';
+import Knob from 'primevue/knob';
+import Listbox from 'primevue/listbox';
+import MegaMenu from 'primevue/megamenu';
+import Menu from 'primevue/menu';
+import Menubar from 'primevue/menubar';
+import Message from 'primevue/message';
+import MultiSelect from 'primevue/multiselect';
+import OrderList from 'primevue/orderlist';
+import OrganizationChart from 'primevue/organizationchart';
+import OverlayPanel from 'primevue/overlaypanel';
+import Paginator from 'primevue/paginator';
+import Panel from 'primevue/panel';
+import PanelMenu from 'primevue/panelmenu';
+import Password from 'primevue/password';
+import PickList from 'primevue/picklist';
+import ProgressBar from 'primevue/progressbar';
+import Rating from 'primevue/rating';
+import RadioButton from 'primevue/radiobutton';
+import Ripple from 'primevue/ripple';
+import SelectButton from 'primevue/selectbutton';
+import ScrollPanel from 'primevue/scrollpanel';
+import ScrollTop from 'primevue/scrolltop';
+import Skeleton from 'primevue/skeleton';
+import Slider from 'primevue/slider';
+import Sidebar from 'primevue/sidebar';
+import SplitButton from 'primevue/splitbutton';
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
+import Steps from 'primevue/steps';
+import TabMenu from 'primevue/tabmenu';
+import TieredMenu from 'primevue/tieredmenu';
+import Textarea from 'primevue/textarea';
+import Toast from 'primevue/toast';
+import Toolbar from 'primevue/toolbar';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import Tag from 'primevue/tag';
+import Timeline from 'primevue/timeline';
+import ToggleButton from 'primevue/togglebutton';
+import Tooltip from 'primevue/tooltip';
+import Tree from 'primevue/tree';
+import TreeSelect from 'primevue/treeselect';
+import TreeTable from 'primevue/treetable';
+import TriStateCheckbox from 'primevue/tristatecheckbox';
+
 
 import PrimeVueLocale from './locale/it/PrimeVue';
 import ToastService from 'primevue/toastservice';
 import ConfirmationService from 'primevue/confirmationservice';
+import BadgeDirective from "primevue/badgedirective";
 
 import 'primevue/resources/themes/saga-blue/theme.css'  //theme
 import 'primevue/resources/primevue.css'            //core css
@@ -37,15 +110,87 @@ createInertiaApp({
       })
       .use(ToastService)
       .use(ConfirmationService)
-      .component('Knob', Knob)
-      .component('DataTable', DataTable)
-      .component('Column', Column)
-      .component('ColumnGroup', ColumnGroup)
-      .component('Row', Row)
-      .component('Chart', Chart)
-      .component('Button', Button)
-      .component('InputText', InputText)
-      .component('Calendar', Calendar)
+        .component('Accordion', Accordion)
+        .component('AccordionTab', AccordionTab)
+        .component('AutoComplete', AutoComplete)
+        .component('Avatar', Avatar)
+        .component('AvatarGroup', AvatarGroup)
+        .component('Badge', Badge)
+        .component('Breadcrumb', Breadcrumb)
+        .component('Button', Button)
+        .component('Calendar', Calendar)
+        .component('Card', Card)
+        .component('Carousel', Carousel)
+        .component('CascadeSelect', CascadeSelect)
+        .component('Chart', Chart)
+        .component('Checkbox', Checkbox)
+        .component('Chip', Chip)
+        .component('Chips', Chips)
+        .component('ColorPicker', ColorPicker)
+        .component('Column', Column)
+        .component('ConfirmDialog', ConfirmDialog)
+        .component('ConfirmPopup', ConfirmPopup)
+        .component('ContextMenu', ContextMenu)
+        .component('DataTable', DataTable)
+        .component('DataView', DataView)
+        .component('DataViewLayoutOptions', DataViewLayoutOptions)
+        .component('Dialog', Dialog)
+        .component('Divider', Divider)
+        .component('Dropdown', Dropdown)
+        .component('Fieldset', Fieldset)
+        .component('FileUpload', FileUpload)
+        .component('Galleria', Galleria)
+        .component('Image', Image)
+        .component('InlineMessage', InlineMessage)
+        .component('Inplace', Inplace)
+        .component('InputMask', InputMask)
+        .component('InputNumber', InputNumber)
+        .component('InputSwitch', InputSwitch)
+        .component('InputText', InputText)
+        .component('Knob', Knob)
+        .component('Listbox', Listbox)
+        .component('MegaMenu', MegaMenu)
+        .component('Menu', Menu)
+        .component('Menubar', Menubar)
+        .component('Message', Message)
+        .component('MultiSelect', MultiSelect)
+        .component('OrderList', OrderList)
+        .component('OrganizationChart', OrganizationChart)
+        .component('OverlayPanel', OverlayPanel)
+        .component('Paginator', Paginator)
+        .component('Panel', Panel)
+        .component('PanelMenu', PanelMenu)
+        .component('Password', Password)
+        .component('PickList', PickList)
+        .component('ProgressBar', ProgressBar)
+        .component('RadioButton', RadioButton)
+        .component('Ripple', Ripple)
+        .component('Rating', Rating)
+        .component('SelectButton', SelectButton)
+        .component('ScrollPanel', ScrollPanel)
+        .component('ScrollTop', ScrollTop)
+        .component('Slider', Slider)
+        .component('Sidebar', Sidebar)
+        .component('Skeleton', Skeleton)
+        .component('SplitButton', SplitButton)
+        .component('Splitter', Splitter)
+        .component('SplitterPanel', SplitterPanel)
+        .component('Steps', Steps)
+        .component('TabMenu', TabMenu)
+        .component('TabView', TabView)
+        .component('TabPanel', TabPanel)
+        .component('Tag', Tag)
+        .component('Textarea', Textarea)
+        .component('TieredMenu', TieredMenu)
+        .component('Timeline', Timeline)
+        .component('Toast', Toast)
+        .component('Toolbar', Toolbar)
+        .component('ToggleButton', ToggleButton)
+        .component('Tooltip', Tooltip)
+        .component('Tree', Tree)
+        .component('TreeSelect', TreeSelect)
+        .component('TreeTable', TreeTable)
+        .component('TriStateCheckbox', TriStateCheckbox)
       .mount(el)
   },
 })
